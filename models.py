5b19ec2f887f70494011946b5097f86c76f319a5
Alma <a.q.frankenstein@gmail.com>
2020-12-21 09:27:14 -0800

    add song_link to model, run db upgrade

--- models.py
+++ models.py
@@ -61,6 +61,7 @@ class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     artist_name = db.Column(db.String(140))
     song_name = db.Column(db.String(140))
+    song_link = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
